e81ef499d8ac3c99a04a8b7838330368d4237d89
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-01-15 23:56:15 +0600

    010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 11, 32, 23, 14, 15, 16, 14, 18 };
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while(index<n)
+{
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
